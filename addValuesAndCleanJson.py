a1711659476db0584befd6fa6ce998a989237cc3
Karch Frankenfield <karchfrankenfield@cloudfitsoftware.com>
2020-04-29 14:04:59 -0400

    fixed some pathing issues

--- addValuesAndCleanJson.py
+++ addValuesAndCleanJson.py
@@ -6,11 +6,11 @@ unclean_output = {}
 
 output['adminPasswordOrKey'] = os.getenv('adminPasswordOrKey')
 
-with open("uncleanOutput.json") as input_file:
+with open("/knab/app/uncleanOutput.json") as input_file:
 	unclean_output = json.load(input_file)
 
 for item in unclean_output:
 	output[item] = unclean_output[item]["value"]
 
-with open("output.json", "w") as output_file:
+with open("/knab/app/output.json", "w") as output_file:
 	json.dump(output, output_file, sort_keys=True, indent=4)
